5d33b008628bc827fde3c0cb6d3744b5d2fc90cf
indiflex <indiflex1@gmail.com>
2025-11-17 11:42:41 +0900

    function ojbect & this

--- js/fn_this.js
+++ js/fn_this.js
@@ -10,8 +10,11 @@ const arrowFn = name => {
   console.log(this, new.target, this.name, name);
 };
 
-// expressFn('expfn');
-// arrowFn(afn');
+const hong = { id: 1, name: 'Hong' };
+const kim = { id: 2, name: 'Kim' };
+
+// expressFn.apply(hong, ['expfn']);
+// arrowFn.call(kim, 'afn');
 
 // const dfn = new expressFn('D');
 // const afn = new arrowFn('A'); // error!
@@ -56,3 +59,66 @@ cat.bark2(); // ?
 console.log('type=', typeof cat); // ?
 
 // cf. FunctionEnvironmentRecord.[[ThisValue]]
+// constructor
+class Man {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  getUserInfo() {}
+}
+
+// const m1 = Man();
+const m2 = new Man();
+
+// factory function
+function createUser(id, name) {
+  return {
+    id: id,
+    name: name,
+    getUserInfo() {
+      return `${this.id}. ${this.name}`;
+    },
+  };
+}
+
+const lee = createUser(3, 'Lee');
+console.log('lee>>', lee.getUserInfo());
+const park = createUser(4, 'Park');
+console.log('park>>', park.getUserInfo());
+console.log('-------------------------');
+globalThis.name = 'Global Name';
+
+let obj = {
+  name: 'Obj Name',
+  printName() {
+    console.log(this.name);
+  },
+};
+
+const printName = obj.printName; // <f.o> address
+obj = null;
+printName();
+// obj.printName(); // Obj Name
+
+console.log('-------------------------');
+const dogx = {
+  name: 'Maxx',
+  showMyName() {
+    console.log(`My name is ${this.name}.`);
+  },
+  whatsYourName() {
+    // setTimeout(this.showMyName, 1000);
+    // setTimeout(showMyName.bind(this), 1000);
+
+    // var self = this;
+    // setTimeout(function() {
+    //   self.showMyName();
+    // }, 1000);
+
+    setTimeout(() => this.showMyName(), 1000);
+  },
+};
+
+dogx.whatsYourName();
