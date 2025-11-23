50d1c3d42477356a8554cfdbd0f566a25c23d35b
indiflex <indiflex1@gmail.com>
2025-11-24 07:40:49 +0900

    promise & randTime

--- js/promi.js
+++ js/promi.js
@@ -7,7 +7,7 @@ function f(cb, delay) {
 function query(sql, cb) {
   try {
     console.log('run:', sql);
-    throw new Error('Error!!');
+    // throw new Error('Error!!');
     const result = [{ id: 1 }];
     cb(null, result);
   } catch (err) {
@@ -16,12 +16,52 @@ function query(sql, cb) {
     console.log('close!');
   }
 }
+function queryPromise(sql) {
+  return new Promise((resolve, reject) => {
+    try {
+      console.log('run:', sql);
+      const result = [{ id: 1 }];
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    } finally {
+      console.log('close!');
+    }
+  });
+}
 
+// class Promise {
+//   #thenFns = [];
+//   #catchFns = [];
+//   #finallyFns = [];
+//   constructor(cb) {
+//     cb(this.resolve, this.reject);
+//   }
+//   resolve(ret) {
+//     let r = ret;
+//     for (const fn of this.#thenFns) {
+//       try {
+//         r = fn(r);
+//       } ct
+//     }
+//     this.#finallyFns...
+//   }
+//   reject(err) {
+//     this.#catchFns(err);
+//   }
+
+//   then(cb) {
+//     this.#thenFns.push(cb);
+//   }
+//   catch(cb) {
+//     this.#catchFns.push(cb);
+//   }
+// }
 const ff = delay =>
   new Promise((resolve, reject) => {
     console.log('connecting....');
-    // setTimeout(() => resolve(), delay);
-    setTimeout(() => reject(new Error('RRR')), delay);
+    setTimeout(() => resolve(111), delay);
+    // setTimeout(() => reject(new Error('RRR')), delay);
   });
 
 // task-queue
@@ -37,7 +77,27 @@ f(
     }),
   1000
 );
-console.log('----------');
+Promise.allSettled([queryPromise('selec1'), queryPromise('select2')]).then(
+  ([r1, r2]) => console.log('🚀 ~ r>>>', r1, r2)
+);
+
+console.log('----------11');
+ff()
+  .then(() => queryPromise('update User..'))
+  .then(result => console.log('result:', result))
+  .then(() => console.log('*************'))
+  .catch(err => console.error(err.message))
+  .catch(console.error)
+  .finally(() => console.log('Finally!!!!!!!!'));
+console.log('----------22');
+const conn = await ff();
+try {
+  const result = await queryPromise('upaaa...');
+  console.log('🚀 ~ result:', result);
+} catch (err) {
+  console.log(err.message);
+}
+console.log('----------33');
 f(() => {
   console.log('yyyyyyy11');
 }, 2000);
@@ -52,3 +112,15 @@ console.log('----------');
 
 // micro-task-queue
 Promise.resolve().then(() => console.log('zzzzzzzz'));
+
+const sampleUrl = 'https://jsonplaceholder.typicode.com/users/1';
+
+// const fetch = new Promise((res, rej) => ...)
+const response = fetch(sampleUrl)
+  .then(res => res.json())
+  .then(res => console.log('%%%>>', res));
+console.log('🚀 ~ response:', response);
+
+const r1 = await fetch(sampleUrl);
+const rrr = await r1.json();
+console.log('🚀 ~ rrr:', rrr);
