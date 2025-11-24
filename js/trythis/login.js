4265a042675cffd655d40660c2d6b24e9c0318fa
indiflex <indiflex1@gmail.com>
2025-11-24 14:46:20 +0900

    dom

---
+++ js/trythis/login.js
@@ -0,0 +1,8 @@
+const $frm = document.getElementById('frm');
+
+$frm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+const $sign_out = document.getElementById('sign-out');
+$sign_out.style.display = 'none';
