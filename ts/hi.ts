37e16a0c3b73c05a230a8d1514d0b87ad557d73a
indiflex <indiflex1@gmail.com>
2025-11-26 11:33:39 +0900

    size_option

--- ts/hi.ts
+++ ts/hi.ts
@@ -12,12 +12,22 @@ const ff: FN = (i: string | number) => {
 ff(1);
 
 const f2 = (i: string | number | boolean) => +i + 1;
-const f4 = (i: string) => +i + 1;
+const f4 = (i: string | number) => +i + 1;
 // cb = f2
 // cb = f4
 function f(cb: FN) {
-  cb(y);
   cb('1');
 }
 f(f2);
 f(f4);
+
+// ---
+const arr = [1, 2, 3];
+if (arr[1]) console.log(arr[0]?.toFixed(1), arr[1] + 100);
+
+type OBJ = { [k: string | number]: number };
+let o1: OBJ = { 1: 1, a: 2 };
+console.log('🚀 ~ o1:', o1);
+const obj: { [x: string]: number } = { id: 1 };
+const a = 'idd';
+console.log('🚀 ~ obj:', obj[a] + 1);
---
+++ ts/trythis/size_option.ts
@@ -0,0 +1,21 @@
+const SIZE = [
+  { id: 'XS', price: 8000 },
+  { id: 'S', price: 10000 },
+  { id: 'M', price: 12000 },
+  { id: 'L', price: 14000 },
+  { id: 'XL', price: 15000 },
+];
+
+const sizeOption1 = { XS: 1, S: 5, M: 2, L: 2, XL: 4 };
+const totalPrice1 = SIZE.reduce(
+  (currPrice, size) => currPrice + sizeOption1[size.id] * size.price,
+  0
+);
+console.log('🚀 ~ totalPrice1:', totalPrice1);
+
+const sizeOption2 = { XS: 2, S: 3, MM: 4, L: 5, XL: 6 };
+const totalPrice2 = SIZE.reduce(
+  (currPrice, size) => currPrice + sizeOption2[size.id] * size.price,
+  0
+);
+console.log('🚀 ~ totalPrice2:', totalPrice2);
