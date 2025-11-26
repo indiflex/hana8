8e40ce3836cb475dd30a97624004bf10f01aad01
indiflex <indiflex1@gmail.com>
2025-11-26 13:38:30 +0900

    trythis - size_options

--- ts/trythis/size_option.ts
+++ ts/trythis/size_option.ts
@@ -1,19 +1,28 @@
+// type X = { id: 'XS' | 'S' | 'M' | 'L' | 'XL'; price: number };
+// const SIZE: X[] = [
+//   { id: 'XS', price: 8000 },
+//   { id: 'S', price: 10000 },
+//   { id: 'M', price: 12000 },
+//   { id: 'L', price: 14000 },
+//   { id: 'XL', price: 15000 },
+// ];
 const SIZE = [
   { id: 'XS', price: 8000 },
   { id: 'S', price: 10000 },
   { id: 'M', price: 12000 },
   { id: 'L', price: 14000 },
   { id: 'XL', price: 15000 },
-];
+] as const;
 
 const sizeOption1 = { XS: 1, S: 5, M: 2, L: 2, XL: 4 };
+// type Q = keyof typeof sizeOption1;
 const totalPrice1 = SIZE.reduce(
   (currPrice, size) => currPrice + sizeOption1[size.id] * size.price,
   0
 );
 console.log('🚀 ~ totalPrice1:', totalPrice1);
 
-const sizeOption2 = { XS: 2, S: 3, MM: 4, L: 5, XL: 6 };
+const sizeOption2 = { XS: 2, S: 3, M: 4, L: 5, XL: 6 };
 const totalPrice2 = SIZE.reduce(
   (currPrice, size) => currPrice + sizeOption2[size.id] * size.price,
   0
