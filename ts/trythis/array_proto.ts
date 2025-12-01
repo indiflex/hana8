7a78d2ccf7748cacca46eb96dcd331ba3390e29b
indiflex <indiflex1@gmail.com>
2025-12-01 13:50:54 +0900

    trythis - args & array.Prototype 2

--- ts/trythis/array_proto.ts
+++ ts/trythis/array_proto.ts
@@ -2,51 +2,120 @@ const hongx = { id: 1, name: 'Hong', dept: 'Server' };
 const kimx = { id: 2, name: 'Kim', dept: 'Server' };
 const leex = { id: 3, name: 'Lee', dept: 'Client' };
 const users = [hongx, leex, kimx];
+
 declare global {
   interface Array<T> {
     firstObject: T;
     lastObject: T;
     // mapBy: (prop: keyof T) => T[];
     mapBy(prop: keyof T): T[];
-    filterBy(prop: keyof T, value: T[keyof T], isIncludes?: boolean): T[];
-    rejectBy(prop: keyof T, value: T[keyof T], isIncludes?: boolean): T[];
+    filterBy<K extends keyof T>(
+      prop: K,
+      value: T[K],
+      isIncludes?: boolean
+    ): T[];
+    rejectBy<K extends keyof T>(
+      prop: K,
+      value: T[K],
+      isIncludes?: boolean
+    ): T[];
+    findBy<K extends keyof T>(prop: K, value: T[K]): T;
+    // sortBy(prop: keyof T | `${keyof T & string}:asc` | `${keyof T & string}:desc`): T[];
+    sortBy(prop: keyof T | `${keyof T & string}:${'asc' | 'desc'}`): T[];
+    groupByFn<K extends T[keyof T] & PropertyKey>(
+      gfn: (a: T) => K
+    ): Partial<Record<K, T[]>>;
+    groupBy<K extends T[keyof T] & PropertyKey>(
+      prop: keyof T
+    ): Partial<Record<K, T[]>>;
   }
 }
-Array.prototype.rejectBy = function <T>(
-  prop: keyof T,
-  value: T[keyof T] & string,
-  isIncludes = false
+Array.prototype.groupByFn = function <T, K extends T[keyof T] & PropertyKey>(
+  gfn: (a: T) => K
 ) {
-  const cb: (a: T) => boolean = isIncludes
-    ? a => typeof a[prop] === 'string' && !a[prop]?.includes(value)
-    : a => a[prop] !== value;
+  const ret: Partial<Record<K, T[]>> = {};
+  for (const a of this) {
+    const k = gfn(a);
+    ret[k] ||= [];
+    ret[k].push(a);
+  }
 
-  return this.filter(cb);
+  return ret;
 };
-console.log(users.rejectBy('id', 2)); // [hong, lee]
-console.log(users.rejectBy('name', 'i', true)); // [hong, lee]
-console.log(users.findBy('name', 'Kim')); //  kim;
+console.log(users.groupByFn(({ dept }) => dept)); // Object.groupBy(users, cb)
+Array.prototype.groupBy = function <T, K extends T[keyof T] & PropertyKey>(
+  prop: keyof T
+) {
+  const ret: Partial<Record<K, T[]>> = {};
+  for (const a of this) {
+    const key: K = a[prop];
+    ret[key] ||= [];
+    ret[key].push(a);
+  }
+  return ret;
+};
+console.log(users.groupBy('dept')); // Object.groupBy(users, cb)
+/*
+Server: [
+  { id: 1, name: 'Hong', dept: 'Server' },
+  { id: 2, name: 'Kim', dept: 'Server' },
+],
+Client: [
+  { id: 3, name: 'Lee', dept: 'Client' }
+],
+*/
+
+Array.prototype.sortBy = function (prop) {
+  // name | name:desc | name:asc | name:
+  // const [key, direction = 'asc'] = prop?.split(':');
+  const [key, direction = 'asc'] =
+    typeof prop === 'string' && prop.includes(':')
+      ? prop.split(':')
+      : [prop, 'asc'];
+  const dir = direction.toLowerCase() === 'desc' ? -1 : 1;
+  return this.sort((a, b) => (a[key] > b[key] ? dir : -dir));
+};
 console.log(users.sortBy('name:desc')); //  [lee, kim, hong];
 console.log(users.sortBy('name')); // [hong, kim, lee]
 
-Array.prototype.filterBy = function <T>(
-  prop: keyof T,
-  value: T[keyof T] & string,
-  isIncludes = false
-) {
-  const cb: (a: T) => boolean = isIncludes
-    ? a => typeof a[prop] === 'string' && a[prop]?.includes(value)
-    : a => a[prop] === value;
-
-  return this.filter(cb);
+Array.prototype.findBy = function (prop, value) {
+  return this.find(a => a[prop] === value);
 };
-// Array.prototype.filterBy = function (prop, value, isIncludes = false) {
-//   const cb = isIncludes
-//   ? a => a[prop]?.includes(value)
-//   : a => a[prop] === value;
+console.log(users.findBy('name', 'Kim')); //  kim;
+
+Array.prototype.rejectBy = function (prop, value, isIncludes = false) {
+  return this.filter(a =>
+    isIncludes ? !a[prop]?.includes(value) : a[prop] !== value
+  );
+};
+console.log(users.rejectBy('id', 2)); // [hong, lee]
+console.log(users.rejectBy('name', 'i', true)); // [hong, lee]
+
+// Array.prototype.filterBy = function <T>(
+//   prop: keyof T,
+//   value: T[keyof T] & string,
+//   isIncludes = false
+// ) {
+//   const cb: (a: T) => boolean = isIncludes
+//     ? a => typeof a[prop] === 'string' && a[prop]?.includes(value)
+//     : a => a[prop] === value;
 
 //   return this.filter(cb);
 // };
+// Array.prototype.filterBy = (prop, value, isIncludes = false) => {
+Array.prototype.filterBy = function (prop, value, isIncludes = false) {
+  return this.filter(a =>
+    isIncludes ? a[prop]?.includes(value) : a[prop] === value
+  );
+  // const cb = (a: typeof this[number]) =>
+  //   isIncludes ? a[prop].includes(value) : a[prop] === value;
+  // // const cb: (a: typeof this[number]) => boolean = isIncludes
+  // // ? a => a[prop]?.includes(value)
+  // // : a => a[prop] === value;
+
+  // return this.filter(cb);
+};
+// console.log(users.filterBy('id', 'Kim')); // Error
 console.log(users.filterBy('id', 2)); // [kim]);
 console.log(users.filterBy('name', 'i', true)); // [kim]
 
@@ -57,38 +126,6 @@ console.log(users.mapBy('id')); // [1, 3, 2];
 console.log(users.mapBy('name')); // ['Hong', 'Lee', 'Kim']);
 
 // --
-Array.prototype.findBy = function (prop, value) {
-  return this.find(a => a[prop] === value);
-};
-
-Array.prototype.sortBy = function (prop) {
-  // name | name:desc | name:asc
-  const [key, direction = 'asc'] = prop?.split(':');
-  const dir = direction.toLowerCase() === 'desc' ? -1 : 1;
-  // console.log('🚀  dir:', dir, prop);
-  return this.sort((a, b) => (a[key] > b[key] ? dir : -dir));
-};
-
-Array.prototype.groupByFn = function (gfn) {
-  const ret = {};
-  for (const a of this) {
-    const k = gfn(a);
-    ret[k] ||= [];
-    ret[k].push(a);
-  }
-
-  return ret;
-};
-Array.prototype.groupBy = function (prop) {
-  const ret = {};
-  for (const a of this) {
-    const key = a[prop];
-    ret[key] ||= [];
-    ret[key].push(a);
-  }
-  return ret;
-};
-
 Object.defineProperties(Array.prototype, {
   firstObject: {
     get() {
@@ -109,17 +146,6 @@ Object.defineProperties(Array.prototype, {
     },
   },
 });
-
-console.log(users.groupBy(({ dept }) => dept));
-/*
-Server: [
-  { id: 1, name: 'Hong', dept: 'Server' },
-  { id: 2, name: 'Kim', dept: 'Server' },
-],
-Client: [
-  { id: 3, name: 'Lee', dept: 'Client' }
-],
-*/
 
 console.log('first/last=', users.firstObject.name, users.lastObject.name); // hong/lee
 users.firstObject = kimx;
