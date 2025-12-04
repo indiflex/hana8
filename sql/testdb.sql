302723036ba42abbc7a38e2ca967cc2365ab7041
indiflex <indiflex1@gmail.com>
2025-12-04 10:43:46 +0900

    create table

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -1,10 +1,27 @@
+use testdb;
+
 show tables;
 
 show processlist;
 kill 11;
 
+desc DeptBackup;
+show index from DeptBackup;
+
 select * from Dept;
+
+create table DeptBackup AS select * from Dept;
+drop table DeptBackup;
+-- truncate table Dept;
+-- Error Code: 1701. Cannot truncate a table referenced in a foreign key constraint (`testdb`.`Emp`, CONSTRAINT `emp_ibfk_1`)
+
+create table EmpBackup AS select * from Emp;
+-- truncate table Emp;
+
+insert into Emp select * from EmpBackup;
+drop table EmpBackup;
 select * from Emp;
+
 select current_user();
 
 create database schooldb;
