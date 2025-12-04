d37182acc8d6a821e5b9f3e3098dc399a4660948
indiflex <indiflex1@gmail.com>
2025-12-04 11:39:44 +0900

    datatype

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -29,3 +29,43 @@ create database schooldb;
 create user school@'%' identified by 'Schooldb1!';
 
 grant all privileges on schooldb.* to school@'%';
+
+select 256 * 256 * 256 * 256;
+
+create table T (
+  id tinyint unsigned auto_increment primary key,
+  name varchar(30) not null,
+  score enum('A', 'B', 'C', 'F') default 'F' comment '학점'
+);
+select * from T;
+desc T;
+insert into T(name, score) values('Hong', 'A');
+insert into T(name, score) values ('Kim', 'B'), ('Lee', 'C');
+insert into T(name) values ('Park');
+
+insert into T(name, score) values('Choi', 'F');
+
+select * from T where score = 4;
+
+alter table T modify column score enum('A', 'B', 'C', 'D', 'F') default 'F' comment '학점';
+
+select * from T where score = 'F';
+
+insert into T(name, score) values('Han', 'D');
+
+select 16 * 1024 * 4;
+select 16382 * 4 + 8; -- 65536
+select 16382 * 4 + 4;
+select 256 * 256;
+
+
+
+
+
+
+
+
+
+
+
+
