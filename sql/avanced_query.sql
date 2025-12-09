5478ef7777c95d5fa193c103266000d8532a48e6
indiflex <indiflex1@gmail.com>
2025-12-10 08:46:25 +0900

    iindex

--- sql/avanced_query.sql
+++ sql/avanced_query.sql
@@ -43,9 +43,87 @@ with recursive fibonacci (n, prev, next) AS (
 )
 select * from fibonacci;
 
-insert into Dept(pid, dname)
-  values(6, '인프라셀'), (6, 'DB셀'), (7, '모바일셀');
+show variables like '%cte%';
+
+insert into Dept(pid, dname) values(6, '인프라셀'), (6, 'DB셀'), (7, '모바일셀');
+insert into Dept(pid, dname) values(3, '영업특공대');
   
 select * from Dept;
-select p.dname, d.dname
-  from Dept p inner join Dept d on p.id = d.pid;
+select p.dname, d.dname, concat(p.id, '-', d.id) h
+  from Dept p inner join Dept d on p.id = d.pid
+ order by h;
+ 
+with recursive CteDept (id, dname, depth, h) AS (
+  select id, dname, 0, cast(id as char(30)) from Dept where pid = 0
+  UNION ALL
+  select d.id, d.dname, cte.depth + 1, concat(cte.h, '-', d.id)
+    from Dept d inner join CteDept cte on d.pid = cte.id
+   where pid = cte.id
+)
+select concat(repeat('↪', depth), dname) from CteDept order by h;
+
+select row_number() over (order by dept, salary desc) '순번', e.*,
+    avg(salary) over w '급여 평균',
+    format(sum(salary) over w, 0) '급여 누적치'
+  from Emp e
+ window w as (partition by dept order by salary desc);
+ 
+select
+    row_number() over(order by dept, salary desc) '순번',
+    e.*,
+    rank() over w '부서내 순위',
+    dense_rank() over w '부서내 순위',
+    percent_rank() over w '부서내 %순위',
+    cume_dist() over w '부서내 %경계',
+    ntile(3) over w '급여'
+  from Emp e
+ where ename like '김%'
+ WINDOW w as (partition by dept order by dept, salary desc);
+ 
+ select row_number() over(order by dept, salary desc) '순번',
+    e.*,
+    sum(salary) over w '급여 누적치',
+    first_value(salary) over w '부서내 1등 급여',
+    last_value(salary) over w '부서내 현재까지의 꼴등 급여',
+    nth_value(salary, 2) over w '부서내 2등 급여',
+    lag(salary, 1) over w '이전 급여',
+    lead(salary, 1) over w '다음 급여'
+  from Emp e
+ where ename like '박%'
+ WINDOW w as (partition by dept order by dept, salary desc);
+
+
+select 
+      (case when p.id is not null then max(p.dname) else '총계' end) pname, 
+      (case when d.id is not null then max(d.dname)
+            when p.id is not null then '-소계-'
+            else ' - ' end) name, 
+      format(sum(e.salary), 0)
+  from Dept p inner join Dept d on p.id = d.pid
+              inner join Emp e on e.dept = d.id
+ group by p.id, d.id
+ with rollup;
+ 
+select d.id, max(d.dname), avg(e.salary), sum(e.salary)
+  from Dept d inner join Emp e on d.id = e.dept
+ group by d.id
+ order by d.id;
+            
+select '평균급여' as '구분', 
+        avg(case when dept = 3 then salary end) as '영업1팀', 
+        avg(if(dept = 4, salary, null)) '영업2팀',
+        avg(if(dept = 5, salary, null)) '영업3팀'
+  from Emp
+UNION
+select '총 급여', 
+        sum(case when dept = 3 then salary end) as '영업1팀', 
+        sum(if(dept = 4, salary, 0)) '영업2팀',
+        sum(if(dept = 5, salary, null)) '영업3팀'
+  from Emp;
+            
+            
+
+            
+            
+            
+            
