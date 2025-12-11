edc2f040c9ac4959fab72e5cbf192292c8974658
indiflex <indiflex1@gmail.com>
2025-12-11 16:53:45 +0900

    sample component 3

--- react/rbvite/src/App.tsx
+++ react/rbvite/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Hello from './components/Hello';
+import My from './components/My';
 
 type Item = {
   id: number;
@@ -8,33 +9,42 @@ type Item = {
   price: number;
   isSoldOut?: boolean;
 };
-type Session = {
-  loginUser: { id: number; name: string; age: number } | null;
+export type LoginUser = { id: number; name: string; age: number };
+export type Session = {
+  loginUser: LoginUser | null;
   cart: Item[];
 };
 
 const DefaultSession = {
   // loginUser: null,
   loginUser: { id: 1, name: 'Hong', age: 33 },
-  cart: [],
+  cart: [
+    { id: 100, name: '라면', price: 3000 },
+    { id: 101, name: '컵라면', price: 2000 },
+    { id: 200, name: '파', price: 5000 },
+  ],
 };
 
 function App() {
   const [count, setCount] = useState(0);
-  const [session] = useState<Session>(DefaultSession);
+  const [session, setSession] = useState<Session>(DefaultSession);
+
+  const logout = () => {
+    // session.loginUser = null; // fail!!
+    setSession({ ...session, loginUser: null });
+  };
 
   return (
     <div className='grid place-items-center h-screen'>
       <h1 className='text-3xl'>count: {count}</h1>
-      <div className='card'>
-        <Hello
-          name={session.loginUser?.name}
-          age={session.loginUser?.age}
-          setCount={setCount}
-        >
-          반갑습니다
-        </Hello>
-      </div>
+      <My session={session} logout={logout} />
+      <Hello
+        name={session.loginUser?.name}
+        age={session.loginUser?.age}
+        setCount={setCount}
+      >
+        반갑습니다
+      </Hello>
     </div>
   );
 }
