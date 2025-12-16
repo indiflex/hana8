a7f5d35248f808b141e7ba0165782a93080a95a0
indiflex <indiflex1@gmail.com>
2025-12-16 09:51:40 +0900

    SessionContext with useReducer

--- react/rbvite/src/hooks/SessionContext.tsx
+++ react/rbvite/src/hooks/SessionContext.tsx
@@ -1,8 +1,8 @@
 import {
   createContext,
   use,
+  useReducer,
   useRef,
-  useState,
   type PropsWithChildren,
   type RefObject,
 } from 'react';
@@ -47,28 +47,64 @@ const SessionContext = createContext<SessionContextValue>({
   saveItem: () => {},
 });
 
+type Action =
+  | { type: 'LOGIN'; payload: LoginUser }
+  | { type: 'LOGOUT'; payload: null }
+  // | { type: 'ADD-ITEM'; payload: Omit<ItemType, 'id'> }
+  | { type: 'ADD-ITEM'; payload: ItemType }
+  | { type: 'EDIT-ITEM'; payload: ItemType }
+  | { type: 'REMOVE-ITEM'; payload: number };
+
+const reducer = (session: Session, { type, payload }: Action) => {
+  switch (type) {
+    case 'LOGIN':
+    case 'LOGOUT':
+      return { ...session, loginUser: payload };
+    case 'ADD-ITEM':
+      return { ...session, cart: [...session.cart, payload] };
+    case 'EDIT-ITEM':
+      return {
+        ...session,
+        cart: session.cart.map((item) =>
+          item.id === payload.id ? payload : item
+        ),
+      };
+    case 'REMOVE-ITEM':
+      return {
+        ...session,
+        cart: session.cart.filter((item) => item.id !== payload),
+      };
+    default:
+      return session;
+  }
+};
+
 export function SessionProvider({ children }: PropsWithChildren) {
-  const [session, setSession] = useState<Session>(DefaultSession);
+  // const [session, setSession] = useState<Session>(DefaultSession);
+  const [session, dispatch] = useReducer(reducer, DefaultSession);
 
   const loginHandlerRef = useRef<LoginHandler>(null);
 
   const logout = () => {
     // session.loginUser = null; // fail!!
-    setSession({ ...session, loginUser: null });
+    // setSession({ ...session, loginUser: null });
+    dispatch({ type: 'LOGOUT', payload: null });
   };
 
   const login: LoginFunction = (name, age) => {
     if (loginHandlerRef.current?.validate())
-      setSession({ ...session, loginUser: { id: 1, name, age } });
+      dispatch({ type: 'LOGIN', payload: { id: 1, name, age } });
+    // setSession({ ...session, loginUser: { id: 1, name, age } });
   };
 
   const removeItem = (id: number) => {
     if (!confirm('Are u sure?')) return;
 
-    setSession({
-      ...session,
-      cart: session.cart.filter((item) => item.id !== id),
-    });
+    // setSession({
+    //   ...session,
+    //   cart: session.cart.filter((item) => item.id !== id),
+    // });
+    dispatch({ type: 'REMOVE-ITEM', payload: id });
   };
 
   const saveItem = ({ id, name, price }: ItemType) => {
@@ -80,19 +116,21 @@ export function SessionProvider({ children }: PropsWithChildren) {
     if (item) {
       // item.name = name;
       // item.price = price;
-      setSession({
-        ...session,
-        cart: session.cart.map((item) =>
-          item.id === id ? { id, name, price } : item
-        ),
-      });
+      // setSession({
+      //   ...session,
+      //   cart: session.cart.map((item) =>
+      //     item.id === id ? { id, name, price } : item
+      //   ),
+      // });
+      dispatch({ type: 'EDIT-ITEM', payload: { id, name, price } });
     } else {
       const newItem = {
         id: Math.max(...session.cart.map((item) => item.id), 0) + 1,
         name,
         price,
       };
-      setSession({ ...session, cart: [...session.cart, newItem] });
+      // setSession({ ...session, cart: [...session.cart, newItem] });
+      dispatch({ type: 'EDIT-ITEM', payload: newItem });
     }
   };
 
