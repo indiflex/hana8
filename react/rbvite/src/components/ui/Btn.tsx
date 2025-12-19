ee856e979483a4cabf6e342143e370c26e6b250f
indiflex <indiflex1@gmail.com>
2025-12-19 16:15:47 +0900

    Btn.stories.tsx

--- react/rbvite/src/components/ui/Btn.tsx
+++ react/rbvite/src/components/ui/Btn.tsx
@@ -4,6 +4,7 @@ import type { PropsWithChildren } from 'react';
 type Prop = {
   onClick?: () => void;
   type?: 'reset' | 'submit';
+  variant?: 'default' | 'destructive' | 'primary';
   disabled?: boolean;
   className?: string;
 };
@@ -12,6 +13,7 @@ export default function Btn({
   onClick,
   type,
   className,
+  variant = 'default',
   disabled,
   children,
 }: PropsWithChildren<Prop>) {
@@ -19,6 +21,10 @@ export default function Btn({
     <button
       type={type}
       className={cn(
+        {
+          'bg-red-500 text-white': variant === 'destructive',
+          'bg-primary-foreground': variant === 'primary',
+        },
         `hover:bg-gray-100 border py-1 px-2 rounded-md cursor-pointer ${className}`
       )}
       onClick={onClick}
