b940e002f4d31a179959f7f38416d90c18b0966e
indiflex <indiflex1@gmail.com>
2025-12-31 13:42:58 +0900

    zod

--- next/my-app/lib/sign.action.ts
+++ next/my-app/lib/sign.action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { AuthError } from 'next-auth';
+import z from 'zod';
 import { signIn, signOut } from './auth';
 
 export type Provider = 'google' | 'github' | 'credentials';
@@ -21,17 +22,61 @@ export type ValidError<T> = {
   data: T;
 };
 
-export type EmailPasswd = { email: string; passwd: string };
+export type EmailPasswd = {
+  email: string | FormDataEntryValue;
+  passwd: string | FormDataEntryValue;
+};
+
+const validate = <T>(zobj: z.ZodObject<T>, formData: FormData) => {
+  const data = Object.fromEntries(formData.entries());
+  // const data = { email, passwd };
+  const validator = zobj.safeParse(data);
+  console.log('🚀 ~ validator:', validator);
+  if (!validator.success) {
+    console.log('****', z.treeifyError(validator.error));
+    const verr = z.treeifyError(validator.error).properties;
+    const validError: ValidError<EmailPasswd> = { error: {}, data };
+
+    for (const [k, v] of Object.entries(verr || [])) {
+      validError.error[k as keyof typeof data] = v.errors[0];
+    }
+
+    return [validError];
+  }
+};
 
 export const loginEmail = async (
   formData: FormData,
 ): Promise<[ValidError<EmailPasswd>] | [undefined, EmailPasswd]> => {
-  const email = formData.get('email') as string;
-  const passwd = formData.get('passwd') as string;
+  const zobj = z.object({
+    email: z.email('Invalid Email Address!'),
+    passwd: z.string().min(3, 'Password is more than 3 characters!'),
+  });
+
+  validate(zobj, formData);
+
+  const { email, passwd } = Object.fromEntries(formData.entries());
   const data = { email, passwd };
+  const validator = zobj.safeParse(data);
+  console.log('🚀 ~ validator:', validator);
+  if (!validator.success) {
+    console.log('****', z.treeifyError(validator.error));
+    const verr = z.treeifyError(validator.error).properties;
+    const validError: ValidError<EmailPasswd> = { error: {}, data };
+
+    for (const [k, v] of Object.entries(verr || [])) {
+      validError.error[k as keyof typeof data] = v.errors[0];
+    }
+
+    return [validError];
+  }
+
+  // const email = formData.get('email') as string;
+  // const passwd = formData.get('passwd') as string;
+  // const data = { email, passwd };
   try {
-    if (!email) return [{ error: { email: 'Input the email!' }, data }];
-    if (!passwd) return [{ error: { passwd: 'Input the passwd!' }, data }];
+    // if (!email) return [{ error: { email: 'Input the email!' }, data }];
+    // if (!passwd) return [{ error: { passwd: 'Input the passwd!' }, data }];
 
     const ret = await signIn('credentials', { redirect: false, email, passwd });
     console.log('🚀 ~ signIn.return:', ret);
