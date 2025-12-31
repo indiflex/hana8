7693508a6f68b78df95cd87eeac59f907e44951b
indiflex <indiflex1@gmail.com>
2025-12-31 14:47:51 +0900

    regist action

--- next/my-app/lib/sign.action.ts
+++ next/my-app/lib/sign.action.ts
@@ -3,6 +3,8 @@
 import { AuthError } from 'next-auth';
 import z from 'zod';
 import { signIn, signOut } from './auth';
+import { isErrorWithMessage } from './errors';
+import { prisma } from './prisma';
 
 export type Provider = 'google' | 'github' | 'credentials';
 
@@ -15,71 +17,39 @@ const login = async (provider: Provider, formData: FormData) => {
   await signIn(provider, { redirectTo });
 };
 
-export type ValidError<T> = {
-  error: {
-    [k in keyof T]?: string;
-  };
-  data: T;
+export type ValidError = {
+  error: Record<string, string | undefined>;
+  data: Record<string, string | undefined>;
 };
 
-export type EmailPasswd = {
-  email: string | FormDataEntryValue;
-  passwd: string | FormDataEntryValue;
-};
-
-const validate = <T>(zobj: z.ZodObject<T>, formData: FormData) => {
-  const data = Object.fromEntries(formData.entries());
-  // const data = { email, passwd };
+const validate = <T extends z.ZodObject>(zobj: T, formData: FormData) => {
+  const data = Object.fromEntries(formData.entries()) as ValidError['data'];
   const validator = zobj.safeParse(data);
-  console.log('🚀 ~ validator:', validator);
   if (!validator.success) {
-    console.log('****', z.treeifyError(validator.error));
-    const verr = z.treeifyError(validator.error).properties;
-    const validError: ValidError<EmailPasswd> = { error: {}, data };
-
-    for (const [k, v] of Object.entries(verr || [])) {
-      validError.error[k as keyof typeof data] = v.errors[0];
+    const verr = z.treeifyError(validator.error).properties || {};
+    const validError: ValidError = { error: {}, data };
+    for (const [k, v] of Object.entries(verr)) {
+      validError.error[k] = v?.errors[0];
     }
-
-    return [validError];
+    return [validError] as const;
   }
+
+  return [undefined, validator.data] as const;
 };
 
-export const loginEmail = async (
-  formData: FormData,
-): Promise<[ValidError<EmailPasswd>] | [undefined, EmailPasswd]> => {
+export const loginEmail = async (formData: FormData) => {
   const zobj = z.object({
     email: z.email('Invalid Email Address!'),
     passwd: z.string().min(3, 'Password is more than 3 characters!'),
   });
 
-  validate(zobj, formData);
+  const [err, data] = validate(zobj, formData);
+  if (err) return [err];
 
-  const { email, passwd } = Object.fromEntries(formData.entries());
-  const data = { email, passwd };
-  const validator = zobj.safeParse(data);
-  console.log('🚀 ~ validator:', validator);
-  if (!validator.success) {
-    console.log('****', z.treeifyError(validator.error));
-    const verr = z.treeifyError(validator.error).properties;
-    const validError: ValidError<EmailPasswd> = { error: {}, data };
-
-    for (const [k, v] of Object.entries(verr || [])) {
-      validError.error[k as keyof typeof data] = v.errors[0];
-    }
-
-    return [validError];
-  }
-
-  // const email = formData.get('email') as string;
-  // const passwd = formData.get('passwd') as string;
-  // const data = { email, passwd };
   try {
-    // if (!email) return [{ error: { email: 'Input the email!' }, data }];
-    // if (!passwd) return [{ error: { passwd: 'Input the passwd!' }, data }];
-
-    const ret = await signIn('credentials', { redirect: false, email, passwd });
+    const ret = await signIn('credentials', { redirect: false, ...data });
     console.log('🚀 ~ signIn.return:', ret);
+
     return [undefined, data];
   } catch (err) {
     console.log('🚀 ~ err:', err, err instanceof AuthError);
@@ -97,3 +67,49 @@ export const loginGoogle = async (formData: FormData) =>
 
 export const loginGithub = async (formData: FormData) =>
   login('github', formData);
+
+export const regist = async (_: ValidError | undefined, formData: FormData) => {
+  const zobj = z
+    .object({
+      name: z.string().min(1).max(30),
+      email: z.email(),
+      passwd: z.string().min(3),
+      passwd2: z.string().min(3),
+    })
+    .refine(
+      ({ passwd, passwd2 }) => passwd === passwd2,
+      'Not equals the passwd and passwd2!',
+    );
+
+  const [err, data] = validate(zobj, formData);
+  console.log('🚀 ~ err:', err, data);
+  if (err) return [err];
+
+  const { email } = data;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user)
+      return [
+        { error: { email: 'This email is already exists!' }, data },
+      ] satisfies [ValidError];
+
+    const newUser = await prisma.user.create({
+      data,
+      select: { id: true, name: true, email: true, isadmin: true },
+    });
+
+    return [undefined, newUser];
+  } catch (err) {
+    return [
+      {
+        error: {
+          email: isErrorWithMessage(err) ? err.message : JSON.stringify(err),
+        },
+        data,
+      },
+    ];
+  }
+};
