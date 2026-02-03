999da04e8a887bdd78737390ac50db665486e4c9
indiflex <indiflex1@gmail.com>
2026-02-04 08:43:01 +0900

    rm

--- java/ttt/src/main/java/com/hana8/ttt/Calc.java
+++
@@ -1,8 +0,0 @@
-package com.hana8.ttt;
-
-public class Calc {
-	public int add(int a, int b) throws Exception {
-		if (a == 0 && b == 0) throw new Exception("Cannot all zero!");
-		return a + b;
-	}
-}
