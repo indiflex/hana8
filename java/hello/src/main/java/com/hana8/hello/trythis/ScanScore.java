35c133c110acc7fa3f3f56d27d2a659f6065065e
indiflex <indiflex1@gmail.com>
2026-02-04 17:34:15 +0900

    day 1

---
+++ java/hello/src/main/java/com/hana8/hello/trythis/ScanScore.java
@@ -0,0 +1,23 @@
+package com.hana8.hello.trythis;
+
+public class ScanScore {
+	public static void main(String[] args) {
+		String[] names = {"Hong", "Kim", "Lee"};
+		int[] scores = {70, 96, 92};
+
+		int totScore = 0;
+		double avgScore = 0;
+		int bestScore = 0;
+		String bestMember = "";
+		for (int i = 0; i < 3; i++) {
+			// scan...
+			String name = names[i];
+			int score = scores[i];
+			totScore += score;
+			if (score > bestScore) {
+				bestScore = score;
+				bestMember = name;
+			}
+		}
+	}
+}
