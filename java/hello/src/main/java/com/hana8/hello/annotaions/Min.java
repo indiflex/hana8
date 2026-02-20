a1ad7ead11554225fabb4490da65260e0bf98b62
indiflex <indiflex1@gmail.com>
2026-02-20 11:42:36 +0900

    trythis - Validate

--- java/hello/src/main/java/com/hana8/hello/annotaions/Min.java
+++ java/hello/src/main/java/com/hana8/hello/annotaions/Min.java
@@ -4,6 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
@@ -11,4 +14,20 @@ public @interface Min {
 	int value();
 
 	String msg() default "%d 보다는 커야합니다!";
+
+	class Validate {
+		public static validate(Field f, Object fval) {
+			Min annotation = f.getAnnotation(Min.class);
+			String fname = f.getName();
+			int value = annotation.value();
+			int v = fval instanceof Number ? ((Number)fval).intValue()
+				: Optional.ofNullable(fval).orElse("").toString().length();
+
+			if (v < value) {
+				String msg = annotation.msg();
+				msgs.computeIfAbsent(fname, k -> new ArrayList<>());
+				msgs.get(fname).add(String.format(msg, value));
+			}
+		}
+	}
 }
--- java/hello/src/main/java/com/hana8/hello/trythis/Reflects.java
+++ java/hello/src/main/java/com/hana8/hello/trythis/Reflects.java
@@ -2,13 +2,71 @@ package com.hana8.hello.trythis;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import com.hana8.hello.annotaions.AnnotationException;
+import com.hana8.hello.annotaions.Max;
+import com.hana8.hello.annotaions.Min;
+import com.hana8.hello.annotaions.NotNull;
 
 public class Reflects {
-	public static void validate(Object obj) {
-		Annotation a = null;
+	public static Map<String, List<String>> validate(Object obj) {
+		Map<String, List<String>> msgs = new HashMap<>();
+
+		try {
+			for (Field f : obj.getClass().getDeclaredFields()) {
+				f.setAccessible(true);
+				Annotation[] annotations = f.getAnnotations();
+				if (annotations.length == 0)
+					continue;
+				// for (Annotation ann : annotations) {
+				// 	System.out.println(f.getName() + " = " + ann.annotationType().getSimpleName());
+				// }
+
+				String fname = f.getName();
+				Object fval = f.get(obj);
+
+				if (fval == null && f.isAnnotationPresent(NotNull.class)) {
+					NotNull annotation = f.getAnnotation(NotNull.class);
+					msgs.computeIfAbsent(fname, k -> new ArrayList<>());
+					msgs.get(fname).add(annotation.value());
+				}
+
+				if (f.isAnnotationPresent(Min.class)) {
+					Min annotation = f.getAnnotation(Min.class);
+					int value = annotation.value();
+					int v = fval instanceof Number ? ((Number)fval).intValue()
+						: Optional.ofNullable(fval).orElse("").toString().length();
+
+					if (v < value) {
+						String msg = annotation.msg();
+						msgs.computeIfAbsent(fname, k -> new ArrayList<>());
+						msgs.get(fname).add(String.format(msg, value));
+					}
+				}
+				if (f.isAnnotationPresent(Max.class)) {
+					Max annotation = f.getAnnotation(Max.class);
+					int value = annotation.value();
+					int v = fval instanceof Number ? ((Number)fval).intValue()
+						: Optional.ofNullable(fval).orElse("").toString().length();
+
+					if (v > value) {
+						String msg = annotation.msg();
+						msgs.computeIfAbsent(fname, k -> new ArrayList<>());
+						msgs.get(fname).add(String.format(msg, value));
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new AnnotationException(e.getMessage());
+		}
+
+		return msgs;
 	}
 
 	public static void makeNotNullFields(Object obj) {
@@ -65,7 +123,10 @@ public class Reflects {
 		Reflects.makeNotNullFields(r);
 		System.out.println("after = " + r);
 
-		Reflection r2 = new Reflection(5, "");
+		System.out.println("============================");
+		Reflection r2 = new Reflection(5, "Kim");
+		// Reflection r2 = new Reflection(null, null);
 		Map<String, List<String>> msgs = Reflects.validate(r2);
+		System.out.println("msgs = " + msgs);
 	}
 }
