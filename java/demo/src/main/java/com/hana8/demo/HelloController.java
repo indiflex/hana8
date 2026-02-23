5794a669c13503d5a23cea34882a177a84cea79c
indiflex <indiflex1@gmail.com>
2026-02-23 16:53:19 +0900

    devtools setting

--- java/demo/src/main/java/com/hana8/demo/HelloController.java
+++ java/demo/src/main/java/com/hana8/demo/HelloController.java
@@ -1,10 +1,25 @@
 package com.hana8.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/hello")
+@RestController
 public class HelloController {
+	@RequestMapping ("/")
+	public String index() {
+		return "Hana8 Springboot Demo!";
+	}
+
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, World!";
+	}
+
+	@GetMapping("/hello-servlet")
+	public String helloServlet(String name) {
+		return "Hello~ " + name + "!!";
 	}
 }
