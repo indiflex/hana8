9e4db15567dd388136e34fa285e1f5fc2dfc5f1a
indiflex <indiflex1@gmail.com>
2026-03-03 11:36:56 +0900

    flyway & unique key

--- java/demo/src/main/java/com/hana8/demo/entity/User.java
+++ java/demo/src/main/java/com/hana8/demo/entity/User.java
@@ -1,5 +1,6 @@
 package com.hana8.demo.entity;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -10,11 +11,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "User", uniqueConstraints = {
+	@UniqueConstraint(
+		name = "uniq_User_email",
+		columnNames = {"email"}
+	),
+	@UniqueConstraint(
+		name = "uniq_User_username_telno",
+		columnNames = {"username", "telno"}
+	)
+})
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,8 +40,11 @@ public class User {
 	@Column(unique = true, nullable = false)
 	private String email;
 
+	@Column(nullable = false, length = 12)
+	private String telno;
+
 	@CreationTimestamp
-	private LocalDateTime createdAt;
+	private Instant createdAt;
 
 	@UpdateTimestamp
 	private LocalDateTime updatedAt;
