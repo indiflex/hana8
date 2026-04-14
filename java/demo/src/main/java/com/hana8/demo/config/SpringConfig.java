367304dddd9a116198bf028c281eff61ea3e631e
indiflex <indiflex1@gmail.com>
2026-04-14 10:18:09 +0900

    348

--- java/demo/src/main/java/com/hana8/demo/config/SpringConfig.java
+++ java/demo/src/main/java/com/hana8/demo/config/SpringConfig.java
@@ -1,42 +1,33 @@
 package com.hana8.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
-import com.hana8.demo.post.PostRepositoryImpl;
-import com.hana8.demo.post.PostRepositoryListImpl;
-import com.hana8.demo.post.PostServiceImpl;
-import com.hana8.demo.post.PostsRepository;
-import com.hana8.demo.post.PostsService;
+import com.hana8.demo.post.*;
 import com.hana8.demo.service.HelpCallService;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @Lazy
 @PropertySources({
-	@PropertySource("classpath:db.properties"),
-	@PropertySource("classpath:mail.properties")
+        @PropertySource("classpath:db.properties"),
+        @PropertySource("classpath:mail.properties")
 })
 public class SpringConfig {
-	@Bean(initMethod = "initialize", destroyMethod = "destroy")
-	public HelpCallService helpCallService() {
-		return new HelpCallService();
-	}
+    @Bean(initMethod = "initialize", destroyMethod = "destroy")
+    public HelpCallService helpCallService() {
+        return new HelpCallService();
+    }
 
-	@Bean
-	public PostsRepository postsRepository() {
-		return new PostRepositoryImpl();
-	}
+    @Bean
+    public PostsRepository postsRepository() {
+        return new PostRepositoryImpl();
+    }
 
-	@Bean
-	public PostsRepository postsRepositoryList() {
-		return new PostRepositoryListImpl();
-	}
+    @Bean
+    public PostsRepository postsRepositoryList() {
+        return new PostRepositoryListImpl();
+    }
 
-	@Bean
-	public PostsService postsService() {
-		return new PostServiceImpl(postsRepository(), postsRepositoryList());
-	}
+    @Bean
+    public PostsService postsService() {
+        return new PostServiceImpl(postsRepository(), postsRepositoryList());
+    }
 }
