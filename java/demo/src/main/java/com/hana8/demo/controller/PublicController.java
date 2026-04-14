797f16e769ed0a5b6e50604a37ed7ec61f662796
indiflex <indiflex1@gmail.com>
2026-04-14 11:20:08 +0900

    api -> apis

--- java/demo/src/main/java/com/hana8/demo/controller/PublicController.java
+++ java/demo/src/main/java/com/hana8/demo/controller/PublicController.java
@@ -1,52 +1,46 @@
 package com.hana8.demo.controller;
 
+import com.hana8.demo.common.enums.Lang;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import org.springframework.context.MessageSource;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import com.hana8.demo.common.enums.Lang;
-
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-
 @RestController
-@RequestMapping("/api/public")
+@RequestMapping("/apis/public")
 @RequiredArgsConstructor
 public class PublicController {
-	private static final String SessLocale = SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME;
-	
-	private final MessageSource ms;
+    private static final String SessLocale = SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME;
 
-	@GetMapping("langs")
-	public Map<String, Object> langs(HttpSession session) {
-		Map<String, Object> res = new LinkedHashMap<>();
-		res.put("Langs", Lang.values());
-		res.put("Locales", Lang.getLangs());
+    private final MessageSource ms;
 
-		Locale currLang = (Locale)session.getAttribute(SessLocale);
-		if (currLang == null)
-			currLang = Locale.getDefault();
-		res.put("currLang", currLang);
+    @GetMapping("langs")
+    public Map<String, Object> langs(HttpSession session) {
+        Map<String, Object> res = new LinkedHashMap<>();
+        res.put("Langs", Lang.values());
+        res.put("Locales", Lang.getLangs());
 
-		res.put("siteTitle", ms.getMessage("site.title", null, currLang));
+        Locale currLang = (Locale) session.getAttribute(SessLocale);
+        if (currLang == null)
+            currLang = Locale.getDefault();
+        res.put("currLang", currLang);
 
-		return res;
-	}
+        res.put("siteTitle", ms.getMessage("site.title", null, currLang));
 
-	@PostMapping("setlang")
-	public ResponseEntity<?> setLang(@RequestHeader("Accept-Language") String lang, HttpSession session) {
-		Locale locale = Locale.forLanguageTag(lang);
-		session.setAttribute(SessLocale, locale);
-		return ResponseEntity.ok((Locale)session.getAttribute(SessLocale));
-	}
+        return res;
+    }
+
+    @PostMapping("setlang")
+    public ResponseEntity<?> setLang(@RequestHeader("Accept-Language") String lang, HttpSession session) {
+        Locale locale = Locale.forLanguageTag(lang);
+        session.setAttribute(SessLocale, locale);
+        return ResponseEntity.ok((Locale) session.getAttribute(SessLocale));
+    }
 
 }
